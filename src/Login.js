358d4c3f53c5c7f4e3ed14cbf64960a537a599c6
Aditi Gupta <70718967+Aditi1025@users.noreply.github.com>
2022-09-10 15:06:55 +0530

    Sign in design

--- src/Login.js
+++ src/Login.js
@@ -49,20 +49,16 @@ function Login() {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+                    <input class='form-input' id='txt-input' type='text' value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+                    <input class='form-input' type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
                     <button type='submit' onClick={signIn} className='login__signInButton'>Sign In</button>
                 </form>
 
-                <p>
-                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
-                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
-                </p>
-
-                <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
+                <button onClick={register} className='login__registerButton'>Create your Account
+                </button>
             </div>
         </div>
     )
